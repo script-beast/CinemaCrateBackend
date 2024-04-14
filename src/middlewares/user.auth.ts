49c4dfd0439f3bbe035edca724f6cf5379f69524
script-beast <ankitkp028@gmail.com>
2024-04-14 21:47:15 +0530

    Trying fixing build error on render

--- src/middlewares/user.auth.ts
+++ src/middlewares/user.auth.ts
@@ -19,11 +19,15 @@ const userAuth = catchAsync(
 
     if (!decoded) return ExpressResponse.unauthorized(res, 'Unauthorized');
 
-    if (typeof decoded === 'string')
-      return ExpressResponse.unauthorized(res, 'Unauthorized');
+    if (typeof decoded === 'string') {
+      ExpressResponse.unauthorized(res, 'Unauthorized');
+      return;
+    }
 
-    if (!mongoose.Types.ObjectId.isValid(decoded.id))
-      return ExpressResponse.unauthorized(res, 'Unauthorized');
+    if (!mongoose.Types.ObjectId.isValid(decoded.id)) {
+      ExpressResponse.unauthorized(res, 'Unauthorized');
+      return;
+    }
 
     const user = await userModel.findById(decoded.id);
 
