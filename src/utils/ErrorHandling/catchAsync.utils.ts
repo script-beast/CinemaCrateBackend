f1491e888877639afa3d4547dc11091b9f99089b
script-beast <ankitkp028@gmail.com>
2024-04-14 21:07:11 +0530

    Trying fixing build error on render

--- src/utils/ErrorHandling/catchAsync.utils.ts
+++ src/utils/ErrorHandling/catchAsync.utils.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 
-const catchAsync = (
+const catchAsync: (
+  fn: (req: Request, res: Response, next: NextFunction) => Promise<void>,
+) => (req: Request, res: Response, next: NextFunction) => void = (
   fn: (req: Request, res: Response, next: NextFunction) => Promise<void>,
 ) => {
   return (req: Request, res: Response, next: NextFunction) => {
